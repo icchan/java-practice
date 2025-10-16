d13fa9d1a75925df37ae54a93057f40a52518676
Ian <ian@oldmac.local>
2025-10-16 16:18:29 +0900

    indent

--- app/src/test/java/org/ian/SolutionTest.java
+++ app/src/test/java/org/ian/SolutionTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
-    @Test void run() {
+    @Test
+    void run() {
         Solution solution = new Solution();
 
         // check it does not throw an exception
         assertDoesNotThrow(() -> solution.run());
-    }    
+    }
 }
