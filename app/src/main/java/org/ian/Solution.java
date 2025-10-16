3865d4c413a23105305a800178c4c1c5aa267332
Ian <ian@oldmac.local>
2025-10-16 16:17:49 +0900

    failed

--- app/src/main/java/org/ian/Solution.java
+++ app/src/main/java/org/ian/Solution.java
@@ -1,13 +1,82 @@
 package org.ian;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This is the solution 
+ * This is the solution
  */
 public class Solution {
     private static final Logger log = LoggerFactory.getLogger(Solution.class);
+
+    Map<String, WorkItem> items = new HashMap<>();
+
+    public void addItem(String key, WorkItem item) {
+        items.put(key, item);
+    }
+
+    public List<WorkItem> find(String field, String value) {
+        List<WorkItem> results = new ArrayList<>();
+
+        for (Entry<String, WorkItem> entry : this.items.entrySet()) {
+            if (field == "key") {
+                if (entry.getKey() == value) {
+                    results.add(entry.getValue());
+                }
+            } else if (field == "project") {
+                if (entry.getValue().project() == value) {
+                    results.add(entry.getValue());
+                }
+            } else if (field == "type") {
+                if (entry.getValue().type() == value) {
+                    results.add(entry.getValue());
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public List<WorkItem> findAnd(ComparisonNode[] comparisons) {
+        // get items for first comparion
+        // iterate the comparisons
+        // AND for each, only keep items in both lists
+        // OR for each, merge lists
+
+        List<WorkItem> results = new ArrayList<>();
+
+        for (ComparisonNode node : comparisons) {
+
+            List<WorkItem> comparisonResult = new ArrayList<>();
+
+            for (Entry<String, WorkItem> entry : this.items.entrySet()) {
+                if (node.getKey() == "key") {
+                    if (entry.getKey() == node.getValue()) {
+                        comparisonResult.add(entry.getValue());
+                    }
+                } else if (node.getKey() == "project") {
+                    if (entry.getValue().project() == node.getValue()) {
+                        comparisonResult.add(entry.getValue());
+                    }
+                } else if (node.getKey() == "type") {
+                    if (entry.getValue().type() == node.getValue()) {
+                        comparisonResult.add(entry.getValue());
+                    }
+                }
+            }
+
+        }
+        // keep only items in all the result lists.
+
+        return null;
+    }
 
     /**
      * Runs the solution
@@ -15,5 +84,42 @@ public class Solution {
     public void run() {
         log.info("ran solution");
 
+    }
+}
+
+// TODO move to a new file
+record WorkItem(String key, String project, String type) {
+}
+
+interface Node {
+}
+
+class ComparisonNode implements Node {
+    private String key;
+    private String value;
+
+    public ComparisonNode(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
+
+class OperationNode implements Node {
+    String operator; // OR or AND
+    Node left;
+    Node right;
+
+    public OperationNode(String operator, Node left, Node right) {
+        this.operator = operator;
+        this.left = left;
+        this.right = right;
     }
 }
--- app/src/test/java/org/ian/SolutionTest.java
+++ app/src/test/java/org/ian/SolutionTest.java
@@ -1,6 +1,9 @@
 package org.ian;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,4 +16,60 @@ public class SolutionTest {
         // check it does not throw an exception
         assertDoesNotThrow(() -> solution.run());
     }
+
+    @Test
+    void easyFind() {
+        Solution solution = new Solution();
+
+        solution.addItem("ABC", new WorkItem("ABC", "ABC-1", "Task"));
+
+        List<WorkItem> result = solution.find("key", "ABC");
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void notFound() {
+        Solution solution = new Solution();
+
+        solution.addItem("ABC", new WorkItem("ABC", "ABC-1", "Task"));
+
+        List<WorkItem> result = solution.find("key", "XYZ");
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void findProject() {
+        Solution solution = new Solution();
+
+        solution.addItem("ABC", new WorkItem("ABC", "ABC-1", "Task"));
+
+        List<WorkItem> result = solution.find("project", "ABC-1");
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void multiFind() {
+        ComparisonNode keySearch = new ComparisonNode("key", "ABC");
+
+        Solution solution = new Solution();
+        solution.addItem("ABC", new WorkItem("ABC", "ABC-1", "Task"));
+
+        ComparisonNode[] query = new ComparisonNode[1];
+        query[0] = keySearch;
+        List<WorkItem> result = solution.findAnd(query);
+
+        assertEquals(1, result);
+
+    }
+    /**
+     * key = ABC AND project = ABC-1
+     * key = ABC OR project = ABC-1
+     * 
+     * key = ABC AND project = ABC-1 OR project = ABC-2
+     * 
+     * 
+     */
 }
